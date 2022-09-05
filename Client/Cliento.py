fa2a7393e598c0aad4baba55ac2caadcf55c4ab1
a20micro <a20micro@student.his.se>
2022-09-05 15:11:45 +0200

    Small updates

--- Client/Cliento.py
+++ Client/Cliento.py
@@ -1,16 +1,49 @@
 import zmq
+import json
+import time
 
 context = zmq.Context()
 # Socket to talk to server
 print("Connecting to server...")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+socket = context.socket(zmq.DEALER)
+
+if socket.connect("tcp://tinyqueue.cognitionreversed.com:5556"):
+
+	queueName = input("Enter your Queue Alias: ")
+
+	ticket = {'enterQueue':True, 'name':queueName}
+	socket.send_json(ticket)
+	message = socket.recv_json()
+
+	if 'ticket' and 'name' in message:
+		print(message['ticket'])
+		print(message['name'])
+
+
+	subReq = {'subscribe':True}
+	socket.send_json(subReq)
+
+	while True:
+
+		message = socket.recv_json()
+
+		if 'queue' and 'supervisors' in message:
+			templist = str(message['queue']).split(',')
+			for i in templist:
+				print(i + "\n")
+
+		if not message:
+			socket.send_json({''})
+
+	#sub = {'subscribe':True}
+
+
 
 # Do 10 requests, waiting each time for response
-for request in range(10):
-    print(f"Sending request {request}...")
-    socket.send_string("Hello")
+#for request in range(10):
+#	print(f"Sending request {request}...")
+#	socket.send_string("Hello")
 
-    # Get reply
-    message = socket.recv()
-    print(f"Receive reply {request} [ {message} ]")
+	# Get reply
+#	message = socket.recv()
+#	print(f"Receive reply {request} [ {message} ]")
