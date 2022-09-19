1f56edbaf5cc57cf543cea772cdde2d094281b21
Gabriel Gomez Palomäki <gabriel.gomez.palomaki@gmail.com>
2022-09-19 22:13:54 +0200

    The name is .py, Servero.py
    Fixat så att man kan ta bort från kön. Använd 'remove' för att ta bort. Notera att True och False spelar ingen roll i detta sammanhang, får den kommando 'remove' så tar den bort den första klienten i kön.

--- Server/Servero.py
+++ Server/Servero.py
@@ -25,7 +25,7 @@ while True:
     
     # If message, get the next request from client
     clientID, message = socket.recv_multipart()
-    print(f'Received request: {message} from {clientID}...')
+    print(f'\nReceived request: {message} from {clientID}...\n')
     
     # First, check if message from new client
     if clientID not in clientIDsList:
@@ -40,12 +40,10 @@ while True:
         if messageDict['enterQueue']:
             # Set to send status to all
             updateStatus = True
-            print('Adding protocol')
             # Should update status for everybody
             updateStatus = True
             # Check if new client wants to join queue
             if not clientQueue:
-                print('Protocol 1.1')
                 # Add first to queue
                 clientQueue.append([clientID, messageDict['name']])     
                 # Create a response to the client for request
@@ -58,15 +56,13 @@ while True:
                 #Send message back to client
                 msg = [clientID, messageRes]
                 socket.send_multipart(msg)
-                print('Server: Client is first to enter queue... New ticket...')
+                print('\tServer: Client is first to enter queue... New ticket...\n')
             else:
                 # Value to determine if match is found
                 clientMatch = False
-                print('Protocol 2.1')
                 for pair in clientQueue:
                     # Check if requesting client is already in queue
                     if clientID in pair:
-                        print('Protocol 2.1.1')
                         # Match found
                         clientMatch = True
                         clientNameMatch = False
@@ -82,78 +78,78 @@ while True:
                                 #Send message back to client
                                 msg = [clientID, messageRes]
                                 socket.send_multipart(msg)
-                                print('Server: Client with that name is already in queue, returned same ticket...')
+                                print('\tServer: Client with that name is already in queue, returned same ticket...\n')
                                 break
                         # Old client uses new alias
                         if not clientNameMatch:    
-                            # Add old client with old name to queue
+                            # Add old client with new name to queue
                             clientQueue.append([clientID, messageDict['name']])     
                             # Create a response to the client for request
-                            ticketDict = {'ticket': clientQueue.index(pair), 'name': messageDict['name']}
+                            ticketDict = {'ticket': len(clientQueue) - 1, 'name': messageDict['name']}
                             queueTickets.append(ticketDict)
                             messageRes = json.dumps(ticketDict)
-                            # Encode message
                             messageRes = str.encode(messageRes)
                             
                             #Send message back to client
                             msg = [clientID, messageRes]
                             socket.send_multipart(msg)
-                            print('Server: Old client and new alias identified... New ticket...')
+                            print('\tServer: Old client and new alias identified... New ticket...\n')
                         break
                 # New client
                 if not clientMatch:
-                    print('Protocol 2.2')
                     # Match found
                     clientNameMatch = False
-                    # Check if old client uses old alias
+                    # Check if new client uses old alias
                     for ticket in queueTickets:
                         if messageDict['name'] == ticket['name']:
                             # Match found
                             clientNameMatch = True
+                            # Add old client with new name to queue
+                            clientQueue.append([clientID, messageDict['name']])     
+                            # Create a response to the client for request
+                            ticketDict = {'ticket': len(clientQueue) - 1, 'name': messageDict['name']}
+                            queueTickets.append(ticketDict)
                             #Prepare message
-                            messageRes = json.dumps(ticket)
+                            messageRes = json.dumps(ticketDict)
                             messageRes = str.encode(messageRes)
                             
                             #Send message back to client
                             msg = [clientID, messageRes]
                             socket.send_multipart(msg)
-                            print('Server: New client, old name identified... New ticket...')
+                            print('\tServer: New client, old name identified... New ticket...\n')
                             break
                     # New client uses new alias
                     if not clientNameMatch:
                         #Add new client with new name to queue
                         clientQueue.append([clientID, messageDict['name']])     
                         # Create a response to the client for request
-                        ticketDict = {'ticket': clientQueue.index(pair), 'name': messageDict['name']}
+                        ticketDict = {'ticket': len(clientQueue) - 1, 'name': messageDict['name']}
                         queueTickets.append(ticketDict)
                         messageRes = json.dumps(ticketDict)
-                        # Encode message
                         messageRes = str.encode(messageRes)
                         
                         #Send message back to client
                         msg = [clientID, messageRes]
                         socket.send_multipart(msg)
-                        print('Server: New client and new alias identified... New ticket...')        
+                        print('\tServer: New client and new alias identified... New ticket...\n')        
 
     # If client want status update
     if 'subscribe' in messageDict:
-        print('My name is Don peo')
         # Check if client want to subscribe
         if messageDict['subscribe']:
             # Check if client is already subscribed
             if clientID not in subsQueue:
                 subsQueue.append(clientID)
-                print('Client successfully subscribed...')
+                print('\t\tServer: Client successfully subscribed...\n')
             
             else:
-                print('Client is already subscribed...')
+                print('\t\tServer: Client is already subscribed...\n')
                 
             # Prepare and send message to client
             statusDict = {'queue' : queueTickets}
             messageRes = json.dumps(statusDict)
             messageRes = str.encode(messageRes)
             msg = [clientID, messageRes]
-            print(msg)
             socket.send_multipart(msg)
         
         # Unsubscribe client
@@ -163,18 +159,41 @@ while True:
                 # If client is subscribed
                 if clientID in subsQueue:
                     subsQueue.pop(subsQueue.index(clientID))
-                    print('Client has been unsubscribed...')
+                    print('\t\tServer: Client has been unsubscribed...\n')
                 
                 else:
-                    print('Client not found in queue...')
-            
-            else:
-                print('No clients are subscribed...')
+                    print('\t\tServer: Client not found in queue...\n')
+
+        print('\t\tServer: Subscriptions has been updated, new total is' + len(subsQueue) + '\n')
     
+    # If remove request is received
+    if 'remove' in messageDict:
+        # Check if queue is empty
+        if not clientQueue:
+            ('\t\t\tServer: Failed to remove from queue... Queue is already empty...')
+        elif clientQueue: 
+            # Check if only one client in queue
+            if len(clientQueue) == 0:
+                # Remove client from queue
+                clientQueue.pop(0)
+                queueTickets.pop(0)
+                updateStatus = True
+            # Check if more than one client in queue
+            elif len(clientQueue) > 0:
+                # Remove client from queue
+                clientQueue.pop(0)
+                queueTickets.pop(0)
+                updateStatus = True
+                # Update remaining clients in queue
+                for ticket in queueTickets:
+                    ticket['ticket'] = queueTickets.index(ticket)
+        # Status update to log
+        print('\t\t\tServer: Changes were made to queue...')
+                    
     # If queue has been updated            
     if updateStatus:
         # For every client in the subscribed list
-        for client in subsQueue:
+        for c in subsQueue:
             # Prepare and send message to client
             statusDict = {'queue' : queueTickets}
             messageRes = json.dumps(statusDict)
@@ -184,12 +203,12 @@ while True:
         # Reset status
         updateStatus = False
         
-    else:
+    if '' == messageDict:
         messageRes = {}
         messageRes = json.dumps(messageRes)
         messageRes = str.encode(messageRes)
         temp = [clientID, messageRes]
-    # Send reply back to client
+        # Send reply back to client
         socket.send_multipart(temp)
     # Do some work
     time.sleep(1)
