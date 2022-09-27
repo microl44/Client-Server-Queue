f112201ec452772200a0ed125bd2ee8e9b11d718
microl44 <71311176+microl44@users.noreply.github.com>
2022-09-27 09:38:35 +0200

    added regular client to git repo

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TBStudentQueue = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBPort = new System.Windows.Forms.TextBox();
+            this.TBSupervisorQueue = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(352, 96);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect To Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnEnterQueue_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // TBStudentQueue
+            // 
+            this.TBStudentQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.TBStudentQueue.Location = new System.Drawing.Point(370, 29);
+            this.TBStudentQueue.Name = "TBStudentQueue";
+            this.TBStudentQueue.Size = new System.Drawing.Size(352, 163);
+            this.TBStudentQueue.TabIndex = 2;
+            this.TBStudentQueue.TabStop = false;
+            this.TBStudentQueue.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Queue Alias:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 96);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Subscribe To Queue";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnSubscribe_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(191, 258);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(173, 96);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Unsubscribe from Queue";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.BtnLeaveQueue);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter Server IP/URL:";
+            // 
+            // TBIP
+            // 
+            this.TBIP.Location = new System.Drawing.Point(120, 31);
+            this.TBIP.Name = "TBIP";
+            this.TBIP.Size = new System.Drawing.Size(211, 20);
+            this.TBIP.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter PORT:";
+            // 
+            // TBPort
+            // 
+            this.TBPort.Location = new System.Drawing.Point(120, 56);
+            this.TBPort.Name = "TBPort";
+            this.TBPort.Size = new System.Drawing.Size(211, 20);
+            this.TBPort.TabIndex = 10;
+            // 
+            // TBSupervisorQueue
+            // 
+            this.TBSupervisorQueue.Location = new System.Drawing.Point(369, 198);
+            this.TBSupervisorQueue.Name = "TBSupervisorQueue";
+            this.TBSupervisorQueue.ReadOnly = true;
+            this.TBSupervisorQueue.Size = new System.Drawing.Size(352, 155);
+            this.TBSupervisorQueue.TabIndex = 11;
+            this.TBSupervisorQueue.TabStop = false;
+            this.TBSupervisorQueue.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 365);
+            this.Controls.Add(this.TBSupervisorQueue);
+            this.Controls.Add(this.TBPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TBIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBStudentQueue);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.RichTextBox TBStudentQueue;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBPort;
+        public System.Windows.Forms.RichTextBox TBSupervisorQueue;
+    }
+}
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Threading;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public static string lastKnownStudentQueue = "";
+        public static string currentStudentQueue = "";
+        private static string currentSupervisorQueue = "";
+        private static List<string> serverList = new List<string>();
+        private static List<string> studentQueue = new List<string>();
+        private static List<string> supervisorList = new List<string>();
+
+        private static string ip;
+        private static string port;
+        private static string username;
+
+        private static bool isConnected;
+        private static bool isInQueue;
+        public Form1()
+        {
+            InitializeComponent();
+            Connecter.socket = new DealerSocket();
+            Connecter.messageToSend = new NetMQMessage();
+            Connecter.messageToRecieve = new NetMQMessage();
+            Connecter.asyncSocket = new NetMQRuntime();
+
+            TBStudentQueue.ReadOnly = true;
+            TBStudentQueue.ShortcutsEnabled = false;
+
+            Thread doSomething = new Thread(() => ThreadWork());
+            doSomething.Start();
+
+            Thread UpdateGUI = new Thread(() => UpdateQueueGUI());
+            UpdateGUI.Start();
+        }
+
+        public class Connecter
+        {
+            public static DealerSocket socket;
+            public static NetMQMessage messageToSend;
+            public static NetMQMessage messageToRecieve;
+            public static NetMQRuntime asyncSocket;
+
+            public static string currentPlace;
+            public static string textBoxText;
+        }
+
+        private void BtnEnterQueue_Click(object sender, EventArgs e)
+        {
+            if (FetchInfo())
+            {
+                string enterQueueTicket = "{\"enterQueue\":true,\"name\":\"" + username + "\"}";
+                SendMessage(enterQueueTicket);
+            }
+        }
+
+        private void BtnSubscribe_Click(object sender, EventArgs e)
+        {
+            if (FetchInfo())
+            {
+                string subscribeToQueue = "{\"subscribe\":true}";
+                SendMessage(subscribeToQueue);
+                isInQueue = true;
+            }
+        }
+
+        public void SendMessage(string MessageToSend)
+        {
+            Connecter.socket.SendFrame(MessageToSend);
+        }
+
+        public bool FetchInfo()
+        {
+            Form popupForm = new Form();
+            RichTextBox popupFormText = new RichTextBox();
+            popupFormText.Width = popupForm.Width - 5;
+            popupFormText.Height = popupForm.Height;
+
+            ip = TBIP.Text;
+            port = TBPort.Text;
+            username = textBox1.Text;
+
+            if (ip == "" || port == "" || TBPort.Text == "" || username == "test")
+            {
+                popupFormText.Text = "One or more textfields were left empty. Please provide appropriate values to each config box.";
+                popupForm.Text = "Empty Field Error";
+                popupFormText.ReadOnly = true;
+                popupForm.Controls.Add(popupFormText);
+                popupForm.Show(this);
+
+                isConnected = false;
+                return false;
+            }
+            else if (Int64.Parse(port) > 65535)
+            {
+                popupFormText.Text = "Port number is too large. Please enter a port number between 1 and 65535";
+                popupForm.Text = "Port Error";
+                popupFormText.ReadOnly = true;
+                popupForm.Controls.Add(popupFormText);
+                popupForm.Show(this);
+
+                isConnected = false;
+                return false;
+            }
+
+            if (username == "")
+            {
+                username = "UNK";
+            }
+            Connecter.socket.Connect("tcp://" + ip + ":" + port);
+            isConnected = true;
+
+            return true;
+        }
+
+        public void ThreadWork()
+        {
+            Form popupForm = new Form();
+            RichTextBox popupFormText = new RichTextBox();
+            popupFormText.Width = popupForm.Width - 5;
+            popupFormText.Height = popupForm.Height;
+
+            while (true)
+            {
+                Connecter.messageToRecieve = Connecter.socket.ReceiveMultipartMessage();
+                var content = Encoding.UTF8.GetString(Connecter.messageToRecieve[0].Buffer);
+
+                JObject jsonContent = JObject.Parse(content);
+
+                if (jsonContent.ContainsKey("ticket") && jsonContent.ContainsKey("name"))// && jsonContent.ContainsKey("serverid"))
+                {
+                    Connecter.currentPlace = jsonContent.GetValue("ticket").ToString();
+                }
+                else if (jsonContent.ContainsKey("queue"))
+                {
+                    extractQueue(jsonContent, "student");
+                }
+                else if (jsonContent.ContainsKey("remove"))
+                {
+                    popupForm.Text = "Message from Supervisor";
+                    popupFormText.Text = jsonContent.GetValue("remove").ToString();
+                    popupFormText.ReadOnly = true;
+                    popupForm.Controls.Add(popupFormText);
+                    popupForm.Show(this);
+                }
+                else if(jsonContent.Count == 1)
+                {
+                    string heartbeat = "{}";
+                    SendMessage(heartbeat);
+                    System.Diagnostics.Debug.WriteLine("updates queue");
+                }
+            }
+        }
+
+        public void changeTextBox(string stringToAdd)
+        {
+            TBStudentQueue.Invoke((MethodInvoker)(() => TBStudentQueue.Text = "---Current Student Queue--- \n " + stringToAdd));
+
+            foreach (string s in studentQueue)
+            {
+                System.Diagnostics.Debug.WriteLine(s);
+            }
+        }
+
+        public void clearTextBox()
+        {
+            TBStudentQueue.Invoke((MethodInvoker)(() => TBStudentQueue.Text = ""));
+        }
+
+        public void extractQueue(JObject Jmessage, string type)
+        {
+            if (Jmessage.ContainsKey("queue") && type == "student")
+            {
+                System.Diagnostics.Debug.WriteLine("updates queue 2 from within type == student");
+                currentStudentQueue = "";
+                studentQueue.Clear();
+
+                foreach (var x in Jmessage.GetValue("queue"))
+                {
+                    studentQueue.Add(x["ticket"].ToString() + " ");
+                    studentQueue.Add(x["name"].ToString() + " \n ");
+                }
+
+                foreach (var s in studentQueue)
+                {
+                    currentStudentQueue += s;
+                }
+            }
+            else if (type == "supervisor")
+            {
+                supervisorList.Clear();
+                currentSupervisorQueue = "";
+
+                foreach (var x in Jmessage.GetValue("supervisor"))
+                {
+                    supervisorList.Add(x["name"].ToString() + " ");
+                    supervisorList.Add(x["status"].ToString() + " \n ");
+                }
+
+                foreach (var x in supervisorList)
+                {
+                    currentSupervisorQueue += x;
+                }
+            }
+
+        }
+
+        public void UpdateQueueGUI()
+        {
+            currentStudentQueue = "";
+            Thread.Sleep(3000);
+            while (true)
+            {
+                if (lastKnownStudentQueue != currentStudentQueue && isInQueue)
+                {
+                    changeTextBox(currentStudentQueue);
+                    lastKnownStudentQueue = currentStudentQueue;
+                }
+            }
+        }
+
+        private void BtnLeaveQueue(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                Thread.Sleep(500);
+                string RemovesubscribeToQueue = "{\"subscribe\":false}";
+                SendMessage(RemovesubscribeToQueue);
+                clearTextBox();
+                currentStudentQueue = "";
+                isInQueue = false;
+            }
+        }
+    }
+}
